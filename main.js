d0137c0d9a1c4908bfa4b9a7ac8e6949d553f3b7
labstraction <45163263+labstraction@users.noreply.github.com>
2023-07-14 09:37:05 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+
+// const canvasCollection = document.getElementsByClassName('my-canvas');
+// const canvasA = canvasCollection[0];
+// console.log(canvasA);
+
+// const canvasCollection2 = document.getElementsByTagName('canvas');
+// const canvasB = canvasCollection2[0];
+// console.log(canvasB);
+
+// const canvasC = document.querySelector('.my-canvas');
+
+// const canvasD = document.querySelector('#canvas1');
+
+// const canvasE = document.querySelector('canvas');
+
+// const canvasCollection3 = document.querySelectorAll('.my-canvas')
+
+const canvas = document.getElementById('canvas1');
+// console.log(canvas.width);
+// console.log(canvas.height);
+const ctx = canvas.getContext('2d');
